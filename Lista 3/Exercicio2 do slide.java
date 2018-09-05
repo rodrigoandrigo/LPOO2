e8c8e2be6f36e4fcd9f5c5160fa4f548efc113c3
Rodrigoandrigo <31012059+rodrigoandrigo@users.noreply.github.com>
2018-09-05 10:02:38 -0300

    Add files via upload

---
+++ Lista 3/Exercicio2 do slide.java
@@ -0,0 +1,82 @@
+package exercicio4;
+
+import javax.swing.JOptionPane;
+
+interface ItemDeBiblioteca {
+
+    void disponivel();
+
+    void emprestado();
+
+    void EmAtraso();
+
+    void reservado();
+}
+
+class Livro implements ItemDeBiblioteca {
+
+    public void disponivel() {
+
+    }
+
+    public void emprestado() {
+
+    }
+
+    public void EmAtraso() {
+
+    }
+
+    public void reservado() {
+
+    }
+
+}
+
+class Revista implements ItemDeBiblioteca {
+
+    public void disponivel() {
+
+    }
+
+    public void emprestado() {
+
+    }
+
+    public void EmAtraso() {
+
+    }
+
+    public void reservado() {
+
+    }
+
+}
+
+class Tese implements ItemDeBiblioteca {
+
+    public void disponivel() {
+
+    }
+
+    public void emprestado() {
+
+    }
+
+    public void EmAtraso() {
+
+    }
+
+    public void reservado() {
+
+    }
+
+}
+
+public class Exercicio4 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
